84474c22049c64e9edf234d5cb9950310b918e76
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-29 03:52:45 -0300

    aprimorando_main

--- main_agenda.rb
+++ main_agenda.rb
@@ -16,22 +16,8 @@ class Main_agenda
       puts("aguarde1 em processo...")
     end
     if x == 2
-      puts("Nome do contato:")
-      filenames = File.new("filenames.txt","a")
-      name=gets
-      filenames.write(name)
-      filenames.close
-      puts("e-mail:")
-      filemails = File.new("filemails.txt","a")
-      mail=gets
-      filemails.write(mail)
-      filemails.close
-      puts("Numero:")
-      filenumbers =  File.new("filenumbers.txt","a")
-      number=gets
-      filenumbers.write(number)
-      filenumbers.close
-      Contato.new(filenames,filemails,filenumbers)
+      user=Contato.new
+      user.add_contato
     end
     if x == 3
       puts("aguarde3 em processo...")
