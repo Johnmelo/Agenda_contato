0382ef63bd9ce9d8307935867a3884309fdd8a69
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-29 04:51:03 -0300

    criando_metodo_edit

--- Contato.rb
+++ Contato.rb
@@ -1,20 +1,34 @@
 class Contato
   def add_contato
     puts("Nome do contato:")
-      filenames = File.new("filenames.txt","a")
-      name=gets
-      filenames.write(name)
-      filenames.close
-      puts("e-mail:")
-      filemails = File.new("filemails.txt","a")
-      mail=gets
-      filemails.write(mail)
-      filemails.close
-      puts("Numero:")
-      filenumbers =  File.new("filenumbers.txt","a")
-      number=gets
-      filenumbers.write(number)
-      filenumbers.close
+    filenames = File.new("filenames.txt","w")
+    name=gets
+    filenames.puts(name)
+    puts("e-mail:")
+    mail=gets
+    filenames.puts(mail)
+    puts("Numero:")
+    number=gets
+    filenames.puts(number)
+    filenames.close
+    return Main_agenda.new
+  end
+  def edit_contato
+    puts("")
+    File.open("filenames.txt") do |filenames|
+      filenames.each_line do |linha|
+	puts linha
+      end
+      puts("")
+    end
+    puts("Editar este contato? 1-SIM 2-NAO")
+    cather=gets
+    cather=cather.to_i
+    if cather==1
+    end
+    if cather==2
+      return Main_agenda.new
+    end
   end
 end
 #calma...to desenrolando :P
