fb3f2d34bc4c49f9c957ae5aba923d25a97367c8
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-29 03:51:44 -0300

    Aprimorando classe

--- Contato.rb
+++ Contato.rb
@@ -1,9 +1,20 @@
 class Contato
-  attr_accessor :list_contact
-  
-  def initialize(nome,email,numero)
-    @list_contact[nome,emal,numero]
+  def add_contato
+    puts("Nome do contato:")
+      filenames = File.new("filenames.txt","a")
+      name=gets
+      filenames.write(name)
+      filenames.close
+      puts("e-mail:")
+      filemails = File.new("filemails.txt","a")
+      mail=gets
+      filemails.write(mail)
+      filemails.close
+      puts("Numero:")
+      filenumbers =  File.new("filenumbers.txt","a")
+      number=gets
+      filenumbers.write(number)
+      filenumbers.close
   end
-    
 end
 #calma...to desenrolando :P
