45904e3f1b34ba2d2d2463faf2d13e668c1a9eb5
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-29 03:11:36 -0300

    aprimorando

--- main_agenda.rb
+++ main_agenda.rb
@@ -1,3 +1,4 @@
+require_relative 'Contato.rb'
 class Main_agenda
   def initialize
     puts("1-VER CONTATOS\n2-ADICIONAR CONTATOS\n3-EDITAR CONTATOS")
@@ -7,7 +8,22 @@ class Main_agenda
       puts("aguarde1 em processo...")
     end
     if x == 2
-      puts("aguarde2 em processo...")
+      puts("Nome do contato:")
+      filenames = File.new("filenames.txt","a")
+      name=gets
+      filenames.write(name)
+      filenames.close
+      puts("e-mail:")
+      filemails = File.new("filemails.txt","a")
+      mail=gets
+      filemails.write(mail)
+      filemails.close
+      puts("Numero:")
+      filenumbers =  File.new("filenumbers.txt","a")
+      number=gets
+      filenumbers.write(number)
+      filenumbers.close
+      Contato.new(filenames,filemails,filenumbers)
     end
     if x == 3
       puts("aguarde3 em processo...")
