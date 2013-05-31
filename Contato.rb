d47af3238ca21f6bda610a425385c5ffa7cb1832
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-31 20:15:21 -0300

    Corrigindo classe

--- Contato.rb
+++ Contato.rb
@@ -10,6 +10,18 @@ class Contato
     $id+=1
     return Main_agenda.new
   end
+  def edit_name(id,name)
+    $lista[id][1] = "NOME:#{name}"
+    return Main_agenda.new
+  end
+  def edit_mail(id,mail)
+    $lista[id][2] = "NOME:#{mail}"
+    return Main_agenda.new
+  end
+  def edit_phone
+    $lista[id][3] = "NOME:#{phone}"
+    return Main_agenda.new
+  end
 end
 
 #calma...to desenrolando :P
