6c9d4250996a9281dc7f91f0fe6d86264ecd0854
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-31 18:14:00 -0300

    Função da classe Contato

--- Contato.rb
+++ Contato.rb
@@ -5,10 +5,11 @@ class Contato
     @name=nome
     @mail=email
     @phone=phone
-    @id=[$id,"NOME:#{name}","EMAIL:#{mail}","NUMERO:#{number}"]
+    @id=[$id,"NOME:#{@name}","EMAIL:#{@mail}","NUMERO:#{@phone}"]
     $lista[$id]+=@id
     $id+=1
     return Main_agenda.new
   end
+end
 
 #calma...to desenrolando :P
