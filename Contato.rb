aec9a1c9cafb89f0d1fe4a3995e8c1e5a3c01063
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-06-01 15:22:36 -0300

    correção de falha

--- Contato.rb
+++ Contato.rb
@@ -3,7 +3,7 @@ require_relative 'Contact_list.rb'
 class Contato
   @@id=0
   def initialize(id,name,mail,phone)
-    @file=["ID:#{$id}","NOME:#{name}","EMAIL:#{mail}","NUMERO:#{phone}"]
+    @file=["ID:#{@@id}","NOME:#{name}","EMAIL:#{mail}","NUMERO:#{phone}"]
     Contact_list.new(@file)
     @@id+=1
   end
