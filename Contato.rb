9e46cd303bb57c747e87d21eac2ce057db4efc41
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-31 23:18:21 -0300

    Corrigido retorno

--- Contato.rb
+++ Contato.rb
@@ -9,19 +9,15 @@ class Contato
     @id=[$id,"NOME:#{@name}","EMAIL:#{@mail}","NUMERO:#{@phone}"]
     $lista[id] = @id
     $id+=1
-    return Main_agenda.new
   end
   def edit_name(id,name)
     $lista[id][1] = "NOME:#{name}"
-    return Main_agenda.new
   end
   def edit_mail(id,mail)
     $lista[id][2] = "NOME:#{mail}"
-    return Main_agenda.new
   end
   def edit_phone
     $lista[id][3] = "NOME:#{phone}"
-    return Main_agenda.new
   end
 end
 
