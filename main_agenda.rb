7e9b83e6a01ad2d32d6461fec90ed3830537af44
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-05-31 23:17:00 -0300

    loop corrigido

--- main_agenda.rb
+++ main_agenda.rb
@@ -1,35 +1,37 @@
 require_relative 'Interaction.rb'
 
-class Main_agenda
-  def initialize
-    puts("##############################################################")
-    puts("##      _______                                            ###")
-    puts("##     /      /  #### #### #### ##   #  ####  ####         ###")
-    puts("##    /  :P  /   #### # ## ##   # #  #  #   # ####         ###")
-    puts("##   /______/    #  # #### #### #   ##  ####  #  #         ###")
-    puts("##                                                         ###")
-    puts("##############################################################")
-    puts("## 1-VER CONTATOS  2-ADICIONAR CONTATOS  3-EDITAR CONTATOS ###")
-    puts("##                                                4-SAIR   ###")
-    puts("##############################################################")
-    x = gets
-    x=x.to_i
-    if x == 1
-      user=Interaction.new
-      user.see_contato
-    end
-    if x == 2
-      user=Interaction.new
-      user.add_contato
-    end
-    if x == 3
-      user=Interaction.new
-      user.edit_contato
-    end
-    if x==4
-      exit
-    end
+#class Main_agenda  
+def menu
+  puts("##############################################################")
+  puts("##      _______                                            ###")
+  puts("##     /      /  #### #### #### ##   #  ####  ####         ###")
+  puts("##    /  :P  /   #### # ## ##   # #  #  #   # ####         ###")
+  puts("##   /______/    #  # #### #### #   ##  ####  #  #         ###")
+  puts("##                                                         ###")
+  puts("##############################################################")
+  puts("## 1-VER CONTATOS  2-ADICIONAR CONTATOS  3-EDITAR CONTATOS ###")
+  puts("##                                                4-SAIR   ###")
+  puts("##############################################################")
+  x = gets
+  x=x.to_i
+  if x == 1
+    user=Interaction.new
+    user.see_contato
+  end
+  if x == 2
+    user=Interaction.new
+    user.add_contato
+  end
+  if x == 3
+    user=Interaction.new
+    user.edit_contato
+  end
+  if x==4
+    exit
   end
 end
 
-Main_agenda.new
+x=0
+while(x!=4)
+  menu
+end
