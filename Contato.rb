2f013af4c2430a8d150b7051e392ea8e2385550e
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-06-01 16:09:52 -0300

    melhorias

--- Contato.rb
+++ Contato.rb
@@ -3,7 +3,8 @@ require_relative 'Contact_list.rb'
 class Contato
   @@id=0
   def initialize(id,name,mail,phone)
-    contato=Contact_list.new("ID:#{@@id}","NOME:#{name}","EMAIL:#{mail}","NUMERO:#{phone}")
+    contato="ID:#{@@id}","NOME:#{name}","EMAIL:#{mail}","NUMERO:#{phone}"
+    Contact_list.new(contato)
     @@id+=1
   end
 end
