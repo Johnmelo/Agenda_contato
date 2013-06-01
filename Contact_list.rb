b997efcb70e0b6758c8f47a809a63040ee720fd0
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-06-01 15:15:39 -0300

    variavel estatica

--- Contact_list.rb
+++ Contact_list.rb
@@ -1,17 +1,20 @@
-$list=[]
 class Contact_list
-  def create(lista)
-    $list = [lista]
+  @@list=[]
+  def initialize(lista)
+    @@list = [lista]
   end
-  def edit_list(id_c,id_a,change)
+  def Contact_list.see
+    return @@list
+  end
+  def Contact_list.edit(id_c,id_a,change)
     if id_a == 1
-      $list[id_c][id_a] = "NOME:#{change}"
+      @@list[id_c][id_a] = "NOME:#{change}"
     end
     if id_a == 2
-      $list[id_c][id_a] = "EMAIL:#{change}"
+      @@list[id_c][id_a] = "EMAIL:#{change}"
     end
     if id_a == 3
-      $list[id_c][id_a] = "PHONE:#{change}"
+      @@list[id_c][id_a] = "PHONE:#{change}"
     end
   end
 end
