6b312c5e68a3a8cd028f77022ff926d6f5dfe3d8
Jonathas Melo <jonathas_ruam@hotmail.com>
2013-06-01 03:36:46 -0300

    Classe Contato

--- Contato.rb
+++ Contato.rb
@@ -1,23 +1,12 @@
-$lista=[]
+require_relative 'Contact_list.rb'
+
 $id=0
 
 class Contato
-  def contato(id,nome,email,phone)
-    @name=nome
-    @mail=email
-    @phone=phone
-    @id=[$id,"NOME:#{@name}","EMAIL:#{@mail}","NUMERO:#{@phone}"]
-    $lista[id] = @id
+  def initialize(id,name,mail,phone)
+    @id=["ID:#{$id}","NOME:#{name}","EMAIL:#{mail}","NUMERO:#{phone}"]
+    Contact_list.new.create(@id)
     $id+=1
-  end
-  def edit_name(id,name)
-    $lista[id][1] = "NOME:#{name}"
-  end
-  def edit_mail(id,mail)
-    $lista[id][2] = "NOME:#{mail}"
-  end
-  def edit_phone
-    $lista[id][3] = "NOME:#{phone}"
   end
 end
 
